8cf439f87ae7b00fbb8271b0698009f9eaf3396f
Reeflex-M <123174183+Reeflex-M@users.noreply.github.com>
2024-12-03 14:41:23 +0100

    Ajout de fichiers de typage pour plusieurs packages et mise à jour de la version d'asgiref

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -101,8 +101,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mistoufles_db',  # Nom de votre base de données MySQL
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',  # ou l'adresse IP du serveur MySQL si différent
+        'PORT': '3306',
     }
 }
 
